63b00778762a447fa2957cf20b7289ae60ec80f5
WilliamChidiacFunctori <william.chidiac@functori.com>
2023-08-21 16:09:29 +0200

    [api] the server and the main program are running asynchronously.

--- src/main/cmd_alchemy.ml
+++ src/main/cmd_alchemy.ml
@@ -2,4 +2,7 @@ open Lwt.Infix
 
 let cmd_alchemy ~verbosity _options =
   Utilities.log ~verbosity "Starting alchemy command@." ;
-  Alchemy_request.request () >>= fun _ -> Lwt.return ()
+  Lwt.async (fun _ ->
+      Alchemy_request.request () >>= fun _ -> Lwt_unix.sleep 1000.) ;
+  Api_server.init_server () ;
+  Lwt.return_unit
