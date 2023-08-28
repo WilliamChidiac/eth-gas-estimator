c3eb38a4af9de8fe07bdbddc5928c11a1e1ff2dd
WilliamChidiacFunctori <william.chidiac@functori.com>
2023-08-28 16:47:05 +0200

    [refactor] replace the useless Lwt.sleep with an Lwt.return_unit.

--- src/main/cmd_alchemy.ml
+++ src/main/cmd_alchemy.ml
@@ -2,7 +2,6 @@ open Lwt.Infix
 
 let cmd_alchemy ~verbosity _options =
   Utilities.log ~verbosity "Starting alchemy command@." ;
-  Lwt.async (fun _ ->
-      Alchemy_request.request () >>= fun _ -> Lwt_unix.sleep 1000.) ;
+  Lwt.async (fun _ -> Alchemy_request.request () >>= fun _ -> Lwt.return_unit) ;
   Api_server.init_server () ;
   Lwt.return_unit
